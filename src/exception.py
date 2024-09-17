0276462bb0dc59b3d4019f170fe3082414d3579c
Md Shamshad <alam.shams.1996@gmail.com>
2024-09-17 10:20:09 +0530

    EDA and data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(erorr, error_detail: sys):
 
@@ -30,6 +30,6 @@ if __name__ =="__main__":
     try:
         a = 1/0
     except Exception as e:
-        logging.info("Divide by zero")
+        logging.info("Divide by zero")  
         raise CustomException(e, sys)
 
