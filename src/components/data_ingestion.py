7fa503b004fc1949f8ec320e2c31bfff88a9914f
Md Shamshad <alam.shams.1996@gmail.com>
2024-09-17 22:41:00 +0530

    prediction pipeline and flask application added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig 
+from src.components.data_transformation_1 import DataTransformation
+from src.components.data_transformation_1 import DataTransformationConfig 
 from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import dill
+import pickle
 from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
@@ -45,5 +46,12 @@ def evaluate_model(X_train,y_train, X_test, y_test, models, params):
             report[list(models.keys())[i]] = test_model_score
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
